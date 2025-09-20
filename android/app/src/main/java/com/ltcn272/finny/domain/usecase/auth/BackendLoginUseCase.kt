71c0cb8be824c9351ea70b3dafa900c1448b1db8
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2025-09-20 18:39:38 +0700

    feat(ios): auth function

--- android/app/src/main/java/com/ltcn272/finny/domain/usecase/auth/BackendLoginUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.ltcn272.finny.domain.usecase.auth
-
-import com.ltcn272.finny.domain.repository.AuthRepository
-
-class BackendLoginUseCase(private val repo: AuthRepository) {
-    suspend operator fun invoke() = repo.backendLogin()
-}
--- android/app/src/main/java/com/ltcn272/finny/domain/usecase/auth/SignInWithFacebookUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.ltcn272.finny.domain.usecase.auth
-
-import com.ltcn272.finny.domain.repository.AuthRepository
-
-class SignInWithFacebookUseCase(private val repo: AuthRepository) {
-    suspend operator fun invoke(accessToken: String) = repo.signInWithFacebook(accessToken)
-}
